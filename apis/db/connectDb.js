fd483063cc5ea575d5a6a69f08d0b9df4e4d9c73
Blaise789 <alainiraguha2016@gmail.com>
2024-05-13 12:17:35 +0200

    changes made

--- apis/db/connectDb.js
+++ apis/db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectToDb=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_DB_URI)
+        await mongoose.connect(process.env.MONGO_DB_URI ,{ useNewUrlParser: true, useUnifiedTopology: true })
         console.log("connectedToDb")
     }
 
