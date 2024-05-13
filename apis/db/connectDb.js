709a5cd77db453ed87e7569976e440b7cad50bf0
Blaise789 <alainiraguha2016@gmail.com>
2024-05-13 15:27:53 +0200

    deprecation removal

--- apis/db/connectDb.js
+++ apis/db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectToDb=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_DB_URI ,{ useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(process.env.MONGO_DB_URI )
         console.log("connectedToDb")
     }
 
